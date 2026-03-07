0abb196ad18cd70b1147dfa6fef94cb52c4c44c4
Srikar Akunoori <srikar.aku02@gmail.com>
2026-03-07 15:20:52 +0530

    Fix Terraform formatting

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
-﻿terraform {
+terraform {
   required_version = ">= 1.7.0"
   required_providers {
     aws = {
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-﻿variable "aws_region" {
+variable "aws_region" {
   description = "AWS region"
   type        = string
   default     = "us-east-1"
