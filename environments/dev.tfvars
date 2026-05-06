9328282d9bd80ee62fcfaac37bddd3d5ffd76262
Srikar Akunoori <srikar.aku02@gmail.com>
2026-05-06 22:42:19 +0530

    added public_subnets

--- environments/dev.tfvars
+++ environments/dev.tfvars
@@ -1,6 +1,6 @@
 aws_region = "ap-south-1"
 env        = "dev"
-vpc_cidr = "10.0.0.0/16"
+vpc_cidr   = "10.0.0.0/16"
 
 public_subnets = [
   "10.0.1.0/24",
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,7 +6,7 @@ resource "aws_subnet" "public" {
   count                   = 2
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnets[count.index]
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
 }
 
