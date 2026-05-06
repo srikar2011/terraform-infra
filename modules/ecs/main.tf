475e5c7821f0defb3a772149aa896752bebbaf64
Srikar Akunoori <srikar.aku02@gmail.com>
2026-05-06 17:01:29 +0530

    corrected terraform formatting

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -48,7 +48,7 @@ resource "aws_ecs_task_definition" "backend" {
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([{
     name  = "backend"
@@ -69,7 +69,7 @@ resource "aws_ecs_task_definition" "frontend" {
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([{
     name  = "frontend"
