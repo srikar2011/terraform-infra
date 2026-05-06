03411f63f576f4e6b4a1472e14c33c12af2f828c
Srikar Akunoori <srikar.aku02@gmail.com>
2026-05-06 16:53:44 +0530

    updated tasks definitions with IAM Roles

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -20,12 +20,35 @@ resource "aws_ecs_cluster" "main" {
   name = "travelmemory-${var.env}"
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole-${var.env}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
+
 resource "aws_ecs_task_definition" "backend" {
   family                   = "backend-${var.env}"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([{
     name  = "backend"
@@ -46,6 +69,7 @@ resource "aws_ecs_task_definition" "frontend" {
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([{
     name  = "frontend"
