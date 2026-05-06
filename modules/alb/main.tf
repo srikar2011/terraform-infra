5bd6cce5bc601aae35eeab380a1a322c0a8d3407
Srikar Akunoori <srikar.aku02@gmail.com>
2026-05-06 18:19:25 +0530

    added target_type

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -28,6 +28,7 @@ resource "aws_lb_target_group" "frontend" {
   port        = 3000
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
+  target_type = "ip"
   
 }
 
@@ -36,6 +37,7 @@ resource "aws_lb_target_group" "backend_blue" {
   port        = 3001
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
+  target_type = "ip"
  
 
   health_check {
@@ -48,6 +50,7 @@ resource "aws_lb_target_group" "backend_green" {
   port        = 3001
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
+  target_type = "ip"
 
 
   health_check {
